524ab9e1e5aa0181ad022f9ac3054bfb8c37dadc
Froncu <henashd@gmail.com>
2023-05-08 17:16:39 +0200

    Update serverstatus-home.js

--- docs/assets/serverstatus-home.js
+++ docs/assets/serverstatus-home.js
@@ -27,4 +27,31 @@
         document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
         {% endif %}
     }
+})
+.then(data => {
+    if (data.online == true) {
+        document.getElementById("serverVermod").innerHTML = `Minecraft ${data.version} Server`;
+
+        // siteNav dependent
+        {% if site.siteNav %}
+        document.getElementById("serverInd").classList.add("text-green-600");
+        document.getElementById("serverPlaynowmod").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        {% else %}
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        {% endif %}
+    }
+    else {
+        document.getElementById("serverPlaynowmod").innerHTML = `<span class="inline-block bg-red-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Currently Offline</span>`;
+        document.getElementById("serverIPmod").classList.remove("bg-green-900");
+        document.getElementById("serverIPmod").classList.add("bg-orange-800");
+
+        // siteNav dependent
+        {% if site.siteNav %}
+        document.getElementById("serverInd").classList.add("text-red-600");
+        {% else %}
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        {% endif %}
+    }
 });
